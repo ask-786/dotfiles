eac25cfd982730e6b6403d2fc181f3f409bfdf05
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2023-08-05 19:58:45 +0530

    updations in nvim, i3 and alacritty

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -11,16 +11,16 @@ lsp_zero.ensure_installed({ 'tsserver', 'eslint', 'lua_ls', 'rust_analyzer' })
 local on_attach = function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set('n', "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set('n', "<leader>e", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set('n', "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set('n', "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set('n', "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set('n', "<leader>gr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set('n', "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set('i', "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-    vim.keymap.set("n", "<leader>dl", function() vim.diagnostic.setqflist() end, opts)
+    vim.keymap.set('n', "gd", function() vim.lsp.buf.definition() end, opts);
+    vim.keymap.set('n', "K", function() vim.lsp.buf.hover() end, opts);
+    vim.keymap.set('n', "<leader>e", function() vim.diagnostic.open_float() end, opts);
+    vim.keymap.set('n', "[d", function() vim.diagnostic.goto_next() end, opts);
+    vim.keymap.set('n', "]d", function() vim.diagnostic.goto_prev() end, opts);
+    vim.keymap.set('n', "<leader>ca", function() vim.lsp.buf.code_action() end, opts);
+    vim.keymap.set('n', "<leader>gr", function() vim.lsp.buf.references() end, opts);
+    vim.keymap.set('n', "<leader>rn", function() vim.lsp.buf.rename() end, opts);
+    vim.keymap.set('i', "<C-a>", function() vim.lsp.buf.signature_help() end, opts);
+    vim.keymap.set("n", "<leader>dg", function() vim.diagnostic.setqflist() end, opts);
 end
 
 lsp_zero.on_attach(on_attach);
@@ -43,11 +43,7 @@ lsp_config['dartls'].setup({
     on_attach = on_attach,
     root_dir = lsp_config.util.root_pattern("pubspec.yaml"),
     init_options = {
-        onlyAnalyzeProjectsWithOpenFiles = false,
-        suggestFromUnimportedLibraries = true,
         closingLabels = true,
-        outline = true,
-        fluttreOutline = true,
     },
     settings = {
         dart = {
--- .config/nvim/lua/ask/packer.lua
+++ .config/nvim/lua/ask/packer.lua
@@ -4,45 +4,49 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	-- Simple plugins can be specified as strings
-	use 'rstacruz/vim-closer'
+    -- Simple plugins can be specified as strings
+    use 'rstacruz/vim-closer'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	use({ 'rose-pine/neovim', as = 'rose-pine' })
-	use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-	use('nvim-treesitter/playground')
-	use('ThePrimeagen/harpoon')
-	use('tpope/vim-fugitive')
-	use('mbbill/undotree')
-    use('jose-elias-alvarez/null-ls.nvim')
-    use('nvim-telescope/telescope-ui-select.nvim')
-    use('christoomey/vim-tmux-navigator')
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			run = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use({ 'rose-pine/neovim', as = 'rose-pine' });
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' });
+    use('nvim-treesitter/playground');
+    use('ThePrimeagen/harpoon');
+    use('tpope/vim-fugitive');
+    use('mbbill/undotree');
+    use('jose-elias-alvarez/null-ls.nvim');
+    use('nvim-telescope/telescope-ui-select.nvim');
+    use('christoomey/vim-tmux-navigator');
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end,
+    }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                 -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate');
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	}
-}
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
+        }
+    }
 end)
