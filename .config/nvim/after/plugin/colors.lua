4a76afda4051cda9300d836847adf94781856d5b
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2023-11-12 14:51:03 +0530

    bla bla

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,9 +1,36 @@
-function ColorMyPencils(color)
-    color = color or "rose-pine";
-    vim.cmd.colorscheme(color);
+require("catppuccin").setup({
+    flavour = "macchiato",                 -- latte, frappe, macchiato, mocha
+    transparent_background = true,         -- disables setting the background color.
+    no_italic = false,                     -- Force no italic
+    no_bold = false,                       -- Force no bold
+    no_underline = false,                  -- Force no underline
+    styles = {                             -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" },           -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+    },
+})
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" });
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" });
-end
-
-ColorMyPencils("catppuccin-macchiato");
+vim.cmd.colorscheme("catppuccin");
