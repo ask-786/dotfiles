769d1cfbddf326b8c29f047a0fa2886459bd021f
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2023-11-12 13:09:45 +0530

    Prrrrrrrrrrr

--- .config/nvim/lua/ask/packer.lua
+++ .config/nvim/lua/ask/packer.lua
@@ -12,11 +12,9 @@ return require('packer').startup(function(use)
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.4',
-        -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
-    use({ 'rose-pine/neovim', as = 'rose-pine' });
     use({ 'catppuccin/nvim', as = 'catppuccin' });
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' });
     use('nvim-treesitter/playground');
