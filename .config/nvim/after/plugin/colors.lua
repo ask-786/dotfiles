a30d134b49d1d1911060253fb478572ed3e99696
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2023-12-05 17:49:49 +0530

    Bored

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,14 +1,20 @@
 require("catppuccin").setup({
-    flavour = "macchiato",         -- latte, frappe, macchiato, mocha
+    flavour = "frappe", -- latte, frappe, macchiato, mocha
+    background = {      -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
     transparent_background = true, -- disables setting the background color.
-    no_italic = false,             -- Force no italic
-    no_bold = false,               -- Force no bold
-    no_underline = false,          -- Force no underline
+    show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
+    term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
         enabled = false,           -- dims the background color of inactive window
         shade = "dark",
-        percentage = 0.01,         -- percentage of the shade to apply to the inactive window
+        percentage = 0.15,         -- percentage of the shade to apply to the inactive window
     },
+    no_italic = false,             -- Force no italic
+    no_bold = false,               -- Force no bold
+    no_underline = false,          -- Force no underline
     styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
         comments = { "italic" },   -- Change the style of comments
         conditionals = { "italic" },
@@ -29,13 +35,18 @@ require("catppuccin").setup({
         cmp = true,
         gitsigns = true,
         nvimtree = true,
+        harpoon = true,
+        mason = true,
         treesitter = true,
         notify = false,
+        fidget = true,
         mini = {
             enabled = true,
             indentscope_color = "",
         },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
     },
 })
 
-vim.cmd.colorscheme("catppuccin");
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin"
