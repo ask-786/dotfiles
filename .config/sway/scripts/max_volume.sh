7f44b16f86153bb50a1572d9fdcdb0ee1fc0a98f
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2024-01-07 11:58:39 +0530

    Now using active volume sink dynamically instead of using anything one specific.

--- .config/sway/scripts/max_volume.sh
+++ .config/sway/scripts/max_volume.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 max_volume_pc=$1
-current_volume_pc=$(pactl list sinks | grep '<Your system language word that means "volume">' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+current_volume_pc=$(pactl list sinks \
+    | grep '<Your system language word that means "volume">' \
+    | head -n $(( $SINK + 1 )) \
+    | tail -n 1 \
+    | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
 
 if (($(echo -n $current_volume_pc | wc -m) == 0)); then
-    current_volume_pc=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+    current_volume_pc=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ \
+        | sed -e 's,.* \([0-9][0-9]*\),\1,' \
+        | awk '{print $1 * 100}')
 fi
 
-if (($current_volume_pc < $max_volume_pc-10)) ; then
+if (($current_volume_pc < $max_volume_pc-5)) ; then
     pactl set-sink-volume @DEFAULT_SINK@ +5% && $refresh_i3status
 else
     a=$(($max_volume_pc - $current_volume_pc))
