39e5fe199c605c3ebf1994e296499ba66dbb7327
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2024-01-07 12:12:05 +0530

    chore: formatting

--- .config/sway/scripts/max_volume.sh
+++ .config/sway/scripts/max_volume.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 max_volume_pc=$1
-current_volume_pc=$(pactl list sinks \
+current_volume_pc=$(\
+    pactl list sinks \
     | grep '<Your system language word that means "volume">' \
     | head -n $(( $SINK + 1 )) \
     | tail -n 1 \
     | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
 
 if (($(echo -n $current_volume_pc | wc -m) == 0)); then
-    current_volume_pc=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ \
+    current_volume_pc=$(
+        wpctl get-volume @DEFAULT_AUDIO_SINK@ \
         | sed -e 's,.* \([0-9][0-9]*\),\1,' \
         | awk '{print $1 * 100}')
 fi
