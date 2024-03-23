8d829a9b58a4c0ab60efd91b2a13c308889baef1
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2024-03-23 08:02:20 +0530

    change.

--- .local/scripts/launchsway.sh
+++ .local/scripts/launchsway.sh
@@ -2,7 +2,7 @@
 export SDL_VIDEODRIVER=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
 export QT_QPA_PLATFORM="wayland;xcb"
-export QT_QPA_PLATFORMTHEME=qt5ct
+export QT_QPA_PLATFORMTHEME=qt6ct
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
 exec sway
